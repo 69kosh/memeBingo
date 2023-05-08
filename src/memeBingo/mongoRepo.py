3cc94089b0e43a3b2aa4daac8a31a0e5ffd64847
69kosh <69kosh@gmail.com>
2023-05-08 15:14:31 +0300

    add almost all code in repo and routes, not tested

--- src/memeBingo/mongoRepo.py
+++ src/memeBingo/mongoRepo.py
@@ -16,24 +16,94 @@ gamesCollection = mongoDb.get_collection('games')
 
 class CardsRepo(AbcCardsRepo):
 
-    def findByTags(self, tags: list[str], limit: int = 100) -> list[str]:
-        docs = cardsCollection.find(filter={'tags': {'$all': tags}} if len(tags) else None,
-                                    projection={'_id': 1},
-                                    sort=[("createdAt", -1)],
-                                    limit=limit)
-        return [doc['_id'] for doc in docs]
+	def findByTags(self, tags: list[str], limit: int = 100) -> list[str]:
+		docs = cardsCollection.find(filter={'tags': {'$all': tags}} if len(tags) else None,
+									projection={'_id': 1},
+									sort=[("createdAt", -1)],
+									limit=limit)
+		return [doc['_id'] for doc in docs]
 
-    def create(self, card: CardModel) -> CardModel | None:
-        res = cardsCollection.insert_one(card.dict(by_alias=True))
-        print(res.inserted_id)
-        return card if res.inserted_id else None
+	def create(self, card: CardModel) -> CardModel | None:
+		res = cardsCollection.insert_one(card.dict(by_alias=True))
+		# print(res.inserted_id)
+		return card if res.inserted_id else None
 
-    def get(self, id:str) -> CardModel | None:
-        data = cardsCollection.find_one({'_id':id})
-        if data:
-            return CardModel.parse_obj(data)
+	def get(self, id: str) -> CardModel | None:
+		data = cardsCollection.find_one({'_id': id})
+		if data:
+			return CardModel.parse_obj(data)
 
-    # def create(self, name: str, isGuest = False) -> UserModel:
-    #     user = UserModel(name=name, isGuest=isGuest)
-    #     usersCollection.insert_one(user.dict(by_alias=True))
-    #     return user
+	def gets(self, ids: list[str]) -> list[CardModel]:
+		docs = cardsCollection.find({'_id': {'$in': ids}})
+		if docs:
+			return [CardModel.parse_obj(doc) for doc in docs]
+
+	def update(self, id: str, card: CardUpdateModel, conditions: dict = {}) -> bool:
+		res = cardsCollection.update_one(
+			filter=conditions | {'_id': id}, update={'$set': card.dict()})
+
+		return res.modified_count == 1
+	
+
+	def hide(self, id: str, conditions: dict = {}) -> bool:
+		res = cardsCollection.update_one(
+			filter=conditions | {'_id': id}, update={'$set': {'hidden': True}})
+
+		return res.modified_count == 1
+
+	def findTagsByTags(self, tags: list[str], limit: int = 100) -> list[str]:
+		result = cardsCollection.aggregate([
+			{'$match': {'tags': {'$all': tags}} if len(tags) else {} },
+			{'$sort': {'createdAt': -1}},
+			{'$limit': limit },
+			{'$project': { '_id': 0, 'tags': 1 } },
+			{'$unwind': "$tags" },
+			{'$group': {'_id': "$tags", 'sum': {'$sum': 1}}},
+			{'$sort': {'sum': -1}}
+		])
+		return [doc['_id'] for doc in result]
+
+	def findByAuthor(self, authorId: str, limit: int = 100) -> list[str]:
+		docs = cardsCollection.find(filter={'authorId': authorId},
+									projection={'_id': 1},
+									sort=[("createdAt", -1)],
+									limit=limit)
+		return [doc['_id'] for doc in docs]
+
+
+class GamesRepo(AbcGamesRepo):
+	
+	def get(self, id: str) -> GameModel:
+		data = gamesCollection.find_one({'_id': id})
+		if data:
+			return GameModel.parse_obj(data)
+	
+	def getMyGamesByCard(self, cardId: str, ownerId: str) -> list[GameModel]:
+		docs = cardsCollection.find(filter={'cardId': cardId, 'ownerId': ownerId},
+									sort=[("createdAt", -1)])
+		return [GameModel.parse_obj(doc) for doc in docs]
+
+	def create(self, game: GameModel) -> GameModel:
+		res = cardsCollection.insert_one(game.dict(by_alias=True))
+		return game if res.inserted_id else None
+	
+	def update(self, id: str, game: GameUpdateModel, conditions: dict = {}) -> bool:
+		res = cardsCollection.update_one(
+			filter=conditions | {'_id': id}, update={'$set': game.dict()})
+
+		return res.modified_count == 1
+	
+	def hide(self, id: str, conditions: dict = {}):
+		res = cardsCollection.update_one(
+			filter=conditions | {'_id': id}, update={'$set': {'hidden' : True}})
+
+		return res.modified_count == 1
+
+	def findCardsByOwner(self, ownerId: str, limit: int = 100) -> list[str]:
+		result = gamesCollection.aggregate([
+			{'$match': {'ownerId': ownerId}},
+			{'$group': {'_id': "$cardId", 'lastGame': {'$max': '$createdAt'}}},
+			{'$sort': {'lastGame': -1}},
+			{'$limit': limit },
+		])
+		return [doc['_id'] for doc in result]
--- src/memeBingo/routes.py
+++ src/memeBingo/routes.py
@@ -8,160 +8,190 @@ from .mongoRepo import *
 
 
 async def getCardsRepo():
-	return CardsRepo()
+    return CardsRepo()
 
 
 async def getGamesRepo():
-	return AbcGamesRepo()
+    return AbcGamesRepo()
 
 
 def mustBeSameUser(subject, request):
-	return subject.get('userId') == request.get('userId')
+    return subject.get('userId') == request.get('userId')
 
 
 router = APIRouter()
 
 
 @router.get("/cards")
-async def listCards(tags: Annotated[list, Query()] = [], limit: int = 20 ,
-					cardsRepo: AbcCardsRepo = Depends(getCardsRepo)) -> list[str]:
-	""" Return list of latest cards, filtered by tags"""
-	return cardsRepo.findByTags(tags=tags, limit=limit)
+async def listCards(tags: Annotated[list, Query()] = [], limit: int = 20,
+                    cardsRepo: AbcCardsRepo = Depends(getCardsRepo)) -> list[str]:
+    """ Return list of latest cards, filtered by tags"""
+    return cardsRepo.findByTags(tags=tags, limit=limit)
 
 
 @router.get("/cards/tags")
 async def listCardsTags(tags: Annotated[list, Query()] = [],
-						cardsRepo: AbcCardsRepo = Depends(getCardsRepo)) -> list[str]:
-	""" Aggregate and return a list of popular tags,
-		according to already selected ones,
-		based on filtered latest cards
-	"""
-	return cardsRepo.findTagsByTags(tags=tags)
+                        cardsRepo: AbcCardsRepo = Depends(getCardsRepo)) -> list[str]:
+    """ Aggregate and return a list of popular tags,
+            according to already selected ones,
+            based on filtered latest cards
+    """
+    return cardsRepo.findTagsByTags(tags=tags)
 
 
 @router.get("/cards/myCards", dependencies=[Depends(checkAccess(mustBeSameUser))])
 async def listMyCards(userId: str, cardsRepo: AbcCardsRepo = Depends(getCardsRepo)) -> list[str]:
-	""" Return list of my cards"""
-	return cardsRepo.findByAuthor(authorId=userId)
+    """ Return list of my cards"""
+    return cardsRepo.findByAuthor(authorId=userId)
 
 
 @router.get("/cards/myGames", dependencies=[Depends(checkAccess(mustBeSameUser))])
 async def listCardsMyGames(userId: str, gamesRepo: AbcGamesRepo = Depends(getGamesRepo)) -> list[str]:
-	""" Return list of cards with my games"""
-	return gamesRepo.findCardsByOwner(authorId=userId)
+    """ Return list of cards with my games"""
+    return gamesRepo.findCardsByOwner(authorId=userId)
 
 
 @router.get("/cards/")
 async def getCards(ids: Annotated[list, Query()] = [],
-				   cardsRepo: AbcCardsRepo = Depends(getCardsRepo)) -> list[CardView]:
-	""" By list of ids return cards"""
-	models = cardsRepo.gets(ids=ids)
-	return [CardView.parse_obj(model.dict()) for model in models if model is not None]
+                   cardsRepo: AbcCardsRepo = Depends(getCardsRepo)) -> list[CardView]:
+    """ By list of ids return cards"""
+    models = cardsRepo.gets(ids=ids)
+    return [CardView.parse_obj(model.dict()) for model in models if model is not None]
 
 
 @router.get("/cards/{id}")
 async def getCard(id: str, cardsRepo: AbcCardsRepo = Depends(getCardsRepo)) -> CardView:
-	model = cardsRepo.get(id=id)
-	return CardView.parse_obj(model.dict())
+    model = cardsRepo.get(id)
+    if model is None:
+        raise HTTPException(
+            status_code=HTTP_404_NOT_FOUND,
+            detail="Not found"
+        )
+    return CardView.parse_obj(model.dict())
 
 
 @router.post("/cards/", dependencies=[Depends(checkAccess(mustBeSameUser))])
 async def createCard(userId: str, card: CardForm,
-					 cardsRepo: AbcCardsRepo = Depends(getCardsRepo)) -> CardView:
-	""" Create new card by owner """
-	model = CardModel.parse_obj(card.dict() | {'authorId': userId})
-	model = cardsRepo.create(card=model)
-	return CardView.parse_obj(model.dict())
+                     cardsRepo: AbcCardsRepo = Depends(getCardsRepo)) -> CardView:
+    """ Create new card by owner """
+    model = CardModel.parse_obj(card.dict() | {'authorId': userId})
+    model = cardsRepo.create(card=model)
+    return CardView.parse_obj(model.dict())
 
 
 @router.post("/cards/{id}/clone", dependencies=[Depends(checkAccess(mustBeSameUser))])
 async def cloneCard(id: str, userId: str,
-					cardsRepo: AbcCardsRepo = Depends(getCardsRepo)) -> CardView:
-	""" Create new card as clone from existed one """
-	parentModel = cardsRepo.get(id=id)
-	model = CardModel.parse_obj(parentModel.dict(
-		exclude=['id', 'createdAt', 'updatedAt', 'authorId', 'parentCardId']) | 
-				{'authorId': userId, 'parentCardId': id})
-	model = cardsRepo.create(card=model)
-	return CardView.parse_obj(model.dict())
+                    cardsRepo: AbcCardsRepo = Depends(getCardsRepo)) -> CardView:
+    """ Create new card as clone from existed one """
+    parentModel = cardsRepo.get(id=id)
+    model = CardModel.parse_obj(parentModel.dict(
+        exclude=['id', 'createdAt', 'updatedAt', 'authorId', 'parentCardId']) |
+        {'authorId': userId, 'parentCardId': id})
+    model = cardsRepo.create(card=model)
+    return CardView.parse_obj(model.dict())
 
 
 @router.put("/cards/{id}", dependencies=[Depends(checkAccess(mustBeSameUser))])
-async def updateCard(id: str, userId: str, card: CardForm, 
-					 cardsRepo: AbcCardsRepo = Depends(getCardsRepo)) -> CardView:
-	""" Update card by owner"""
-	model = CardUpdateModel.parse_obj(card.dict())
-	model = cardsRepo.update(id, model, conditions={'authorId': userId})
-	# check userId == model.authorId
-	if model is None:
-		raise HTTPException(
-			status_code=HTTP_403_FORBIDDEN,
-			detail="Not authorized"
-		)
-	return CardView.parse_obj(model.dict())
+async def updateCard(id: str, userId: str, card: CardForm,
+                     cardsRepo: AbcCardsRepo = Depends(getCardsRepo)) -> CardView:
+    """ Update card by owner"""
+    model = CardUpdateModel.parse_obj(card.dict())
+    success = cardsRepo.update(id, model, conditions={'authorId': userId})
+    # check userId == model.authorId
+    if success is None:
+        raise HTTPException(
+            status_code=HTTP_403_FORBIDDEN,
+            detail="Not authorized"
+        )
+    model = cardsRepo.get(id=id)
+    return CardView.parse_obj(model.dict())
 
 
 @router.delete("/cards/{id}", dependencies=[Depends(checkAccess(mustBeSameUser))])
-async def hideCard(id: str, userId: str, 
-		   cardsRepo: AbcCardsRepo = Depends(getCardsRepo)):
-	""" Hide card by owner"""
-	success = cardsRepo.hide(id, conditions={'authorId': userId})
-	# check userId == model.authorId
-	if not success:
-		raise HTTPException(
-			status_code=HTTP_403_FORBIDDEN,
-			detail="Not authorized"
-		)
+async def hideCard(id: str, userId: str,
+                   cardsRepo: AbcCardsRepo = Depends(getCardsRepo)):
+    """ Hide card by owner"""
+    success = cardsRepo.hide(id, conditions={'authorId': userId})
+    # check userId == model.authorId
+    if not success:
+        raise HTTPException(
+            status_code=HTTP_403_FORBIDDEN,
+            detail="Not authorized"
+        )
 
 
 @router.get("/cards/{id}/isOwner/{userId}")
-async def isCardOwner(id: str, userId: str, 
-		   cardsRepo: AbcCardsRepo = Depends(getCardsRepo)) -> bool:
-	""" Check card owner """
-	model = cardsRepo.get(id)
-	if model is None:
-		raise HTTPException(
-			status_code=HTTP_404_NOT_FOUND,
-			detail="Not found"
-		)
-	return model.authorId == userId
+async def isCardOwner(id: str, userId: str,
+                      cardsRepo: AbcCardsRepo = Depends(getCardsRepo)) -> bool:
+    """ Check card owner """
+    model = cardsRepo.get(id)
+    if model is None:
+        raise HTTPException(
+            status_code=HTTP_404_NOT_FOUND,
+            detail="Not found"
+        )
+    return model.authorId == userId
 
 
 @router.get("/cards/{id}/canShare")
 async def canShareCard(id: str) -> bool:
-	""" Check card for share, no one was supposed to play the card """
-	return True
-
-
-@router.get("/games/{id}")
-async def getGame(id: str) -> GameView:
-	return GameView()
+    """ Check card for share, the card author must not be a guest"""
+    return True
 
 
 @router.get("/games/{id}/canShare")
 async def canShareGame(id: str) -> bool:
-	return True
+    """ Check game for share, the card author must not be a guest"""
+    return True
+
+
+@router.get("/games/{id}")
+async def getGame(id: str, gamesRepo: AbcGamesRepo = Depends(getGamesRepo)) -> GameView:
+    model = gamesRepo.get(id)
+    if model is None:
+        raise HTTPException(
+            status_code=HTTP_404_NOT_FOUND,
+            detail="Not found"
+        )
+    return GameView.parse_obj(model.dict())
 
 
 @router.post("/games/", dependencies=[Depends(checkAccess(mustBeSameUser))])
-async def startGame(userId: str, cardId: str) -> GameView:
-	""" Start game by card """
-	return GameView()
+async def startGame(userId: str, cardId: str, gamesRepo: AbcGamesRepo = Depends(getGamesRepo)) -> GameView:
+    """ Start game by card """
+    model = GameModel.parse_obj({'authorId': userId, 'cardId': cardId})
+    model = gamesRepo.create(game=model)
+    return GameView.parse_obj(model.dict())
 
 
 @router.put("/games/{id}", dependencies=[Depends(checkAccess(mustBeSameUser))])
-async def updateGame(id: str, userId: str, gameForm: GameForm):
-	""" Update game by owner"""
-	return GameView()
+async def updateGame(id: str, userId: str, gameForm: GameForm, gamesRepo: AbcGamesRepo = Depends(getGamesRepo)):
+    """ Update game by owner"""
+    model = GameUpdateModel.parse_obj(gameForm.dict())
+    success = gamesRepo.update(id, model, conditions={'ownerId': userId})
+    # check userId == model.ownerId
+    if success is None:
+        raise HTTPException(
+            status_code=HTTP_403_FORBIDDEN,
+            detail="Not authorized"
+        )
+    model = gamesRepo.get(id=id) # а надо ли оно?
+    return GameView.parse_obj(model.dict())
 
 
 @router.delete("/games/{id}", dependencies=[Depends(checkAccess(mustBeSameUser))])
-async def hideGame(id: str, userId: str):
-	""" Hide game by owner"""
-	...
-
+async def hideGame(id: str, userId: str, gamesRepo: AbcGamesRepo = Depends(getGamesRepo)):
+    """ Hide game by owner"""
+    success = gamesRepo.hide(id, conditions={'ownerId': userId})
+    # check userId == model.authorId
+    if not success:
+        raise HTTPException(
+            status_code=HTTP_403_FORBIDDEN,
+            detail="Not authorized"
+        )
 
 @router.get("/games/byCard/{cardId}", dependencies=[Depends(checkAccess(mustBeSameUser))])
-async def listMyGamesOfCard(cardId: str, userId: str) -> list[GameView]:
-	""" Return list of my games by card"""
-	return []
+async def listMyGamesOfCard(cardId: str, ownerId: str, gamesRepo: AbcGamesRepo = Depends(getGamesRepo)) -> list[GameView]:
+    """ Return list of my games by card"""
+    models = gamesRepo.getMyGamesByCard(cardId=cardId, ownerId=ownerId)
+    return [GameView.parse_obj(model.dict()) for model in models if model is not None]
